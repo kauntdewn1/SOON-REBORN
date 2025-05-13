2f60bf716b15ecdd0c7866b49bdac71570cbac4d
mello.flowreborn <mello.flowreborn@gmail.com>
2025-05-13 05:39:27 +0000

    new OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,22 @@ export const metadata: Metadata = {
     },
     shortcut: "https://res.cloudinary.com/dgyocpguk/image/upload/v1746323807/favicon_pvncfw.png",
   },
+  openGraph: {
+    title: 'FLOW//REBORN', // Título para Open Graph
+    description: 'Prepare-se para o sistema de sobrevivência definitivo.', // Descrição para Open Graph
+    url: 'https://res.cloudinary.com/dgyocpguk/image/upload/v1745697577/previa_og8mlz.png', // << Substitua por sua URL real
+    siteName: 'FLOW//REBORN', // Nome do seu site
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dgyocpguk/image/upload/v1745697577/previa_og8mlz.png', // URL da imagem que você forneceu
+        width: 1200, // Largura da imagem (ideal para OG)
+        height: 630, // Altura da imagem (ideal para OG)
+        alt: 'Prévia do site FLOW//REBORN', // Texto alternativo da imagem
+      },
+    ],
+    locale: 'pt_BR', // Opcional: define o idioma
+    type: 'website', // Tipo de conteúdo
+  },
 };
 
 export default function RootLayout({
